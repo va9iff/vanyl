eccdab711297df4dc6c60ad58b4b7173aaed2c9e
va9iff <va9iff@gmail.com>
2023-03-13 07:56:18 +0400

    little changes over erros

--- src/expect.js
+++ src/expect.js
@@ -1,15 +1,14 @@
 export class VError extends Error {
 	name = "VanylError"
 }
-export class TypeVError extends TypeError {
+export class VTypeError extends TypeError {
 	name = "VanylTypeError"
 }
-
 
 export const expect = {
 	vResult(arg) {
 		if (!(arg instanceof VResult))
-			throw new TypeVError("expected vResult. \n instead use v`` function.")
+			throw new VTypeError("expected vResult. \n use v`` function.")
 	},
 	sameVResult(vResult1, vResult2) {
 		if (!vResult1.isSame(vResult2)) throw new VError("expected same vResult")
@@ -31,7 +30,7 @@ export const expect = {
 	},
 	instanceof(instance, cls) {
 		if (!(instance instanceof cls))
-			throw new TypeVError(`expected an instance of ${cls.name || cls}`)
+			throw new VTypeError(`expected an instance of ${cls.name || cls}`)
 	},
 	vResultAt(arg, i) {
 		if (!(arg instanceof VResult))
